318af3c213e74ee7513d68ea61febf7a22543e86
TSoli <tariqsoliman2000@gmail.com>
2024-10-10 13:17:19 +1000

    Connections now stay open

--- controller/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    PICO_W_ADDRESS = "http://192.168.10.1"
-    
--- robot/test/test_comms.py
+++ robot/test/test_comms.py
@@ -1,3 +1,6 @@
+import json
+import socket
+
 from comms import connect, open_socket
 from logger import logger
 
@@ -8,19 +11,30 @@ def main() -> None:
     port = 80
     logger.info(f"Setting up socket on {ip}:{port}")
     sock = open_socket(ip, port)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
     while True:
         cl, addr = sock.accept()
         logger.info(f"Client connected from: {addr}")
+        while True:
+            header = cl.recv(1024).decode()
+            if not header:
+                cl.close()
+                logger.info(f"Closing connection to {addr}")
+                break
 
-        request = cl.recv(1024)
-        print("Request:", request)
+            content_length = int(header.split("Content-Length: ")[1].split("\r\n")[0])
+            body = cl.recv(content_length).decode()
+            # headers, body = request.split("\r\n\r\n", 1)
+            data = json.loads(body)
+            logger.info(f"Header: {header}")
+            logger.info(f"Request:{data}")
 
-        # Prepare response
-        response = "HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\nHello!\n"
-        cl.send(response.encode())
-
-        cl.close()
+            message = "Hello!"
+            # Prepare response
+            headers = f"HTTP/1.0 200 OK\r\nContent-type: text/plain\r\nContent-Length: {len(message)}\r\nConnection: keep-alive\r\n\r\n"
+            cl.send(headers.encode())
+            cl.send(message.encode())
 
 
 if __name__ == "__main__":
