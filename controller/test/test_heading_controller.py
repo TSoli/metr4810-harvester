c32e851ffa7860d59cd81e2a9c955a1474caf2b5
Tariq <87625599+TSoli@users.noreply.github.com>
2024-10-20 19:48:23 +1000

    tested pi controller (#10)

--- controller/test/test_heading_controller.py
+++ controller/test/test_heading_controller.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import sys
+import time
 
 import cv2
 import numpy as np
@@ -10,7 +11,7 @@ parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, parent_directory)
 
 from comms import Comms
-from localisation import (ROBOT_MARKERS, Localisation,
+from localisation import (ROBOT_MARKERS, BufferlessVideoCapture, Localisation,
                           extract_pose_from_transform, get_cam_params)
 from logger import logger
 from path_following import HeadingController
@@ -21,14 +22,7 @@ DEFAULT_SIZE = "DICT_4X4_50"
 def main(args=None):
     dev = args.device
 
-    cap = cv2.VideoCapture(dev)
-    if not cap.isOpened():
-        logger.warning("Could not open camera")
-        exit(1)
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap = BufferlessVideoCapture(dev)
 
     mtx, dist = get_cam_params(args.cam_params)
 
@@ -37,7 +31,7 @@ def main(args=None):
         mtx, dist, ROBOT_MARKERS, marker_size=args.square, dict_type=args.size
     )
 
-    hc = HeadingController(0.5, 0.005)
+    hc = HeadingController(1, 0.4)
 
     # comms
     with open(os.path.join("..", "comms_config.json")) as f:
@@ -46,15 +40,14 @@ def main(args=None):
     ip = data["ip"]
     comms = Comms(ip)
     # put the scoop up
-    comms.send_scoop_request(True)
+    # comms.send_scoop_request(True)
 
-    goal_headings = [np.pi, 0, -np.pi / 2]
+    goal_headings = [-np.pi / 4, 0, np.pi, -np.pi / 2]
     for goal in goal_headings:
         logger.info(f"Attempting to reach heading: {goal:.2f}")
+        hc.reset()
         while True:
-            ret, frame = cap.read()
-            if not ret:
-                continue
+            frame = cap.read()
 
             tf_wr = loc.localise(frame)
             if tf_wr is None:
@@ -65,10 +58,19 @@ def main(args=None):
             if w == 0:
                 break
 
-            comms.send_drive_request(0, w)
+            cv2.imshow("frame", frame)
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                break
+
+            comms.send_drive_request(0.0, w)
+            time.sleep(0.02)
 
         logger.info("Goal Reached!")
-        comms.send_drive_request(0, 0)
+        comms.send_drive_request(0.0, 0.0)
+        comms.send_drive_request(0.0, 0.0)
+        comms.send_drive_request(0.0, 0.0)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
