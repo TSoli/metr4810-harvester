588ddd67221ff29b71ab3fb357f63b306e45f3ce
Connor Davis <cpdavis.2003@gmail.com>
2024-10-20 19:46:57 +1000

    The good controller

--- controller/controller_2.py
+++ controller/controller_2.py
@@ -318,11 +318,17 @@ def generate_straight_line(start, stop, spacing = 0.05):
     # Generate the x and y points
     x_points = np.linspace(start[0], stop[0], num_lines)
     y_points = np.linspace(start[1], stop[1], num_lines)
-    heading = math.atan2((stop[1] - start[1])/(stop[0] - start[0]))
+    heading = math.atan2((stop[1] - start[1]), (stop[0] - start[0]))
+
+    # Adjust to be within the correct range
+    heading -= math.pi / 2 
 
     # Adjust the heading to be between 0 and 2pi
     if heading < 0:
         heading += 2 * math.pi
+
+    # convert to degrees
+    heading = math.degrees(heading)
 
     # Combine the points so that it is (x, y, heading)
     points = np.column_stack((x_points, y_points, np.ones(num_lines) * heading))
