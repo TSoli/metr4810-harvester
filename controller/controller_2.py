eb3d72d75b7364a9f615b8512b058ec934546df5
Connor Davis <cpdavis.2003@gmail.com>
2024-10-20 19:13:40 +1000

    remove some random stuff

--- controller/controller_2.py
+++ controller/controller_2.py
@@ -318,7 +318,11 @@ def generate_straight_line(start, stop, spacing = 0.05):
     # Generate the x and y points
     x_points = np.linspace(start[0], stop[0], num_lines)
     y_points = np.linspace(start[1], stop[1], num_lines)
-    heading = math.tan((stop[1] - start[1])/(stop[0] - start[0]))
+    heading = math.atan2((stop[1] - start[1])/(stop[0] - start[0]))
+
+    # Adjust the heading to be between 0 and 2pi
+    if heading < 0:
+        heading += 2 * math.pi
 
     # Combine the points so that it is (x, y, heading)
     points = np.column_stack((x_points, y_points, np.ones(num_lines) * heading))
